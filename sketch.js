2e7fb22a6359d2d35eb97e40758731b621c1962f
minhuaren030-LAN <minhuaren030@gmail.com>
2026-01-19 23:03:49 +0000

    upload practice

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+function keyPressed(){
+  
+if(key =="d")
+  textSize(10)
+  fill(0)
+text("d is pressed",100,50)
+fill(0)
+circle(200,200,30);
+}
